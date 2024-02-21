c8faa8cd284abe9ccd896971636955bc01365ef1
Tom Young <33696649+tomyoung1@users.noreply.github.com>
2024-02-21 17:25:56 +0000

    added min electrolyser output and whole-year run

--- hoptimiser/variable_price.py
+++ hoptimiser/variable_price.py
@@ -32,6 +32,7 @@ class MultiDimensionalLpVariable:
         f = np.vectorize(lambda i: pulp.value(i))
         self.values = f(self.variables)
 
+start_time = datetime.datetime.now()
 
 input_file_name = r'C:\Users\tyoung\Documents\GitHub\hydrogen-optimisation-research\examples\Example_demand.csv'
 
@@ -41,26 +42,34 @@ data['Hour'] = data.Time.dt.hour
 data['Day'] = data.Time.dt.date
 
 starting_storage_kwh = 0#5000
-max_storage_kwh = 50000
-storage_max_charge_rate_kw = 20000
+max_storage_kwh = 30000
+storage_max_charge_rate_kw = 5000
 
 electrolyser_max_power = 20000
-electrolyser_min_power = 1000 #todo work out how to include this
+electrolyser_min_power = 3000
 electrolyser_efficiency = 0.70
 
+electrolyser_min_power = max(electrolyser_min_power, 1E-4)
+
 max_h2_production_kwh = electrolyser_max_power * electrolyser_efficiency * 0.5
+min_h2_production_kwh = electrolyser_min_power * electrolyser_efficiency * 0.5
 max_h2_to_storage_kwh = min(storage_max_charge_rate_kw * 0.5, max_h2_production_kwh)
 
 total_cost = 0
 day_start_h2_in_storage_kwh = starting_storage_kwh
 
-for day in data['Day'].unique():
+results_df = pd.DataFrame(columns=['price','h2_demand_kWh','h2_produced_kWh','h2_to_storage_kWh','h2_in_storage_kWh'])
+
+day_results_df = results_df.copy()
+
+for day in data['Day'].unique()[0:len(data['Day'].unique())-1]:
 
     h2_in_storage_kwh = day_start_h2_in_storage_kwh
 
-    print(day)
-
     data_day = data.loc[((data['Day'] == day) & (data['Hour'] >= 11)) | (data['Day'] == day + datetime.timedelta(days=1)), :]
+
+    #data_following_day_repeat = data.loc[(data['Day'] == day + datetime.timedelta(days=1)), :]
+    #data_day = pd.concat([data_day, data_following_day_repeat])
 
     data_day = data_day.reset_index()
 
@@ -71,10 +80,9 @@ for day in data['Day'].unique():
 
     h2_produced_kWh = MultiDimensionalLpVariable('h2_produced_kWh', len(data_day), 0, max_h2_production_kwh, "Continuous")
     h2_to_storage = MultiDimensionalLpVariable('h2_to_storage', len(data_day), -max_h2_from_storage_kwh, max_h2_to_storage_kwh, "Continuous")
+    electrolyser_turned_on = MultiDimensionalLpVariable('electrolyser_turned_on', len(data_day), 0, 1, cat = "Binary")
 
     problem = LpProblem("Minimize_energy_cost_while_meeting_demand", LpMinimize)
-
-    #objective function
 
     h2_price_array = price_array / electrolyser_efficiency
 
@@ -93,40 +101,51 @@ for day in data['Day'].unique():
 
         demand_shortfall = demand_array[i] - h2_produced_kWh[i] + h2_to_storage[i]
         storage_overuse = -h2_to_storage[i] - h2_in_storage_kwh
-        #storage_overfill = h2_to_storage[i] - h2_storage_space_available_kwh
+        storage_overfill = h2_to_storage[i] - h2_storage_space_available_kwh
 
         h2_in_storage_kwh = h2_in_storage_kwh + h2_to_storage[i]
 
         problem += demand_shortfall == 0
         problem += storage_overuse <= 0
-        #problem += storage_overfill <= 0
+        problem += storage_overfill <= 0
 
-    problem.solve(PULP_CBC_CMD(msg=True, keepFiles=False, timeLimit = 1.0))
+        problem += h2_produced_kWh[i] <= electrolyser_turned_on[i] * max_h2_production_kwh
+        problem += h2_produced_kWh[i] >= electrolyser_turned_on[i] * min_h2_production_kwh
 
-    print(problem.objective.value()/1000000)
+    problem.solve(PULP_CBC_CMD(msg=False, keepFiles=False, timeLimit = 100.0))
 
     h2_produced_kWh.evaluate()
     h2_to_storage.evaluate()
 
     this_day_cost = 0
     h2_in_storage_tracker = day_start_h2_in_storage_kwh
+    h2_in_storage = np.zeros(48)
+    cost_array = np.zeros(48)
 
     for i in range(0,48):
-        this_day_cost += h2_produced_kWh.values[i] * h2_price_array[i]
+        cost_array[i] = h2_produced_kWh.values[i] * h2_price_array[i]
         h2_in_storage_tracker = h2_in_storage_tracker + h2_to_storage.values[i]
+        h2_in_storage[i] = h2_in_storage_tracker
 
     day_start_h2_in_storage_kwh = h2_in_storage_tracker
 
-    print(day_start_h2_in_storage_kwh)
+    total_cost += cost_array.sum()
 
-    total_cost += this_day_cost
+    day_results_df['price'] = price_array[0:48]
+    day_results_df['h2_demand_kWh'] = demand_array[0:48]
+    day_results_df['h2_produced_kWh'] = h2_produced_kWh.values[0:48]
+    day_results_df['h2_to_storage_kWh'] = h2_to_storage.values[0:48]
+    day_results_df['h2_in_storage_kWh'] = h2_in_storage[0:48]
+    day_results_df['h2_cost'] = cost_array[0:48]
 
-    print(h2_produced_kWh.values)
-    print(h2_to_storage.values)
-    print(h2_produced_kWh.values - h2_to_storage.values)
+    results_df = pd.concat([results_df, day_results_df])
 
-    #todo we need to extract h2_in_storage at 11am the following day, not at the end of the forecast period. Do this by looping through steps again using the known control logic
+print(total_cost/1000000)
 
+results_df.to_csv('results.csv')
 
-print(total_cost)
+end_time = datetime.datetime.now()
 
+time_taken = end_time - start_time
+
+print('Time taken = ', time_taken)
